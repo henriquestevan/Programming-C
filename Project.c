e2b01d74436dc047f6d6c9d2029ff356ce716af8
henriquestevan <106773384+henriquestevan@users.noreply.github.com>
2024-04-08 14:48:06 -0300

    Update Project.c

--- Project.c
+++ Project.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <locale.h>
-#include <windows.h>
 
 int main()
 {
@@ -9,25 +8,27 @@ int main()
     int vet[25] = {4, 0, 2, 0, 8, 9, 9, 8, 6, 4, 0, 2, 0, 6, 8, 4, 9, 0, 9, 8, 2, 6, 8, 8, 4};
     int qnt_0 = 0, qnt_2 = 0, qnt_4 = 0, qnt_6 = 0, qnt_8 = 0, qnt_9 = 0;
     int correctPassword[6] = {6, 4, 3, 5, 8, 6};
-    int i, mov, pos=10, tentativas=0;
+    int i, mov, pos=10, tentativas=0, deslizamentos=0;
 
     printf("Olá, para abrir esse cofre você deverá adivinhar a senha!!\n");
-    sleep(1);
 
     printf("A senha é composta de 6 dígitos, e para descobri-la, você deverá se deslocar por essa barra escolhendo a posição que\n");
     printf("deseja ir, começando pela posição 10(começa na posição 0, ou seja, 0/1/2...24),\n");
     printf("4, 0, 2, 0, 8, 9, 9, 8, 6, 4, ( 0 ), 2, 0, 6, 8, 4, 9, 0, 9, 8, 2, 6, 8, 8, 4");
     printf("\nSua senha será a quantidade de vezes que cada número aparece em cada movimento");
+    printf("\nVocê só terá 3 tentativas!!");
 
     printf("\nDigite a posição que deseja ir:");
     scanf("%d", &mov);
 
     do
     {
-     if(mov>25 || mov<0)
+
+     if(mov>24 || mov<0)
      {
-         printf("\nPosição inválida");
-         return 1;
+         printf("\nPosição inválida\n");
+         exit(1);
+
      }
 
      if( mov>10)
@@ -97,10 +98,10 @@ int main()
     tentativas++;
     }while(tentativas<3);
 
-    printf("%d, %d, %d, %d, %d, %d, %d, %d", pos, mov, qnt_0, qnt_2, qnt_4, qnt_6, qnt_8, qnt_9);
+
+    printf("%d, %d, %d, %d, %d, %d", qnt_0, qnt_2, qnt_4, qnt_6, qnt_8, qnt_9);
 
 
     return 0;
 
 }
-
